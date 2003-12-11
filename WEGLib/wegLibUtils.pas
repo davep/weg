b667a4d931625659c79a3044831cfbd0cc457e47
davep <>
2003-12-11 13:01:38 +0000

    Version 1.1 of WEGLib.

--- WEGLib/wegLibUtils.pas
+++ WEGLib/wegLibUtils.pas
@@ -43,7 +43,7 @@ Const
   {** ID of a menu entry }
   wegLib_ENTRY_MENU = 2;
   {** Version of WEGLib }
-  wegLib_VERSION = '1.0';
+  wegLib_VERSION = '1.1';
 
 Type
   {** Types of reads that can take place }
