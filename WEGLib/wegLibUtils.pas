4e58c88f18f8a7e5f394d6e603180304c013cf17
davep <>
2004-03-05 15:16:48 +0000

    v1.7 of WEGLib.

--- WEGLib/wegLibUtils.pas
+++ WEGLib/wegLibUtils.pas
@@ -43,7 +43,7 @@ Const
   {** ID of a menu entry }
   wegLib_ENTRY_MENU = 2;
   {** Version of WEGLib }
-  wegLib_VERSION = '1.6';
+  wegLib_VERSION = '1.7';
 
 Type
   {** Types of reads that can take place }
